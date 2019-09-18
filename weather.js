7248c8bc85b118debfc2b422f589a8916dbe3484
bluepks3000 <30798525+bluepks3000@users.noreply.github.com>
2019-09-19 05:43:22 +0900

    완료

--- weather.js
+++ weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "028f815b6e87f8f919e8cbd6374df199";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    }) //데이터가 다 들어올 때까지 기다리는 함수
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature}@${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +29,7 @@ function handleGeoSucces(position) {
     longitude
   }; //객체의 변수와 key 이름이 같을 때 이렇게 쓸 수 있음.
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -24,10 +42,11 @@ function askForCoords() {
 
 function loadCoords() {
   const loadedCoords = localStorage.getItem(COORDS);
-  if (loadCoords == null) {
+  if (loadedCoords == null) {
     askForCoords();
   } else {
-    //getweather
+    const parsedCoords = JSON.parse(loadedCoords); //string을 값으로(?) 변형. ex) "hi" → hi
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
